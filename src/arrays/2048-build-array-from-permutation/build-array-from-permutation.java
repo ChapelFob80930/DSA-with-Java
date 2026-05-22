7bfd465cdaf2fa64e08319cf28d9d31e04504fd1
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-22 12:35:59 +0530

    Time: 1 ms (99.45%) | Memory: 47.3 MB (31.23%) - LeetSync

---
+++ src/arrays/2048-build-array-from-permutation/build-array-from-permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        int i = 0;
+        for(int num: nums){
+            ans[i] = nums[num];
+            i++;
+        }
+        return ans;
+    }
+}
