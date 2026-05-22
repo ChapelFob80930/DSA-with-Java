17c72116de07ecdda1d767b5d27a5b334c98ff54
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-22 12:14:38 +0530

    Time: 61 ms (1.24%) | Memory: 46.6 MB (99.66%) - LeetSync

---
+++ src/arrays/566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+
+        // A 2D matrix is stored row-by-row like a linear 1D sequence in memory.
+        // Example:
+        // 1 2
+        // 3 4
+        // becomes -> [1, 2, 3, 4]
+        //
+        // So instead of moving elements manually,
+        // we use the same linear index (idx) in both matrices.
+        //
+        // oldRow = idx / oldCols
+        // oldCol = idx % oldCols
+        //
+        // newRow = idx / newCols
+        // newCol = idx % newCols
+        //
+        // Changing only the number of columns automatically
+        // gives the new reshaped position while preserving order.
+
+        if(r*c != mat.length*mat[0].length){ //check if total number of elements is the same
+            return mat;
+        }
+
+        int[][] ans = new int[r][c];
+
+        for(int i = 0; i<mat.length*mat[0].length; i++){ //iterate over all elements
+
+            int oldRow = i/mat[0].length;
+            int oldCol = i%mat[0].length;
+            int newRow = i/c;
+            int newCol = i%c;
+
+            System.out.println(mat[oldRow][oldCol]);
+
+            ans[newRow][newCol] = mat[oldRow][oldCol];
+
+            System.out.println(ans[newRow][newCol]);
+        }
+        
+
+        return ans;
+    }
+}
