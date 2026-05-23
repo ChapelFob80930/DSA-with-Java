50329a9ea406d599008a973994fe9474635f2295
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-23 13:43:49 +0530

    Time: 1 ms (76.53%) | Memory: 46.5 MB (90.10%) - LeetSync

--- src/arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ src/arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -18,7 +18,7 @@ class Solution {
         }
 
                                                                                             
-        System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(nums));
                                                                                                                                 
         return unique;
 
