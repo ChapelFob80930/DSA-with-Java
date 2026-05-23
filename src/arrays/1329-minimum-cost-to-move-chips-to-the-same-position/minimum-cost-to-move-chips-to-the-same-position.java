3ecb9c7e925b8089cb3271d3188f7f29a096698d
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-23 14:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (52.65%) - LeetSync

---
+++ src/arrays/1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int cost = 0;
+        int odd = 0;
+        int even = 0;
+        for(int i =0; i<position.length; i++){
+            if(position[i]%2==0){
+                even ++;
+            }
+            else{
+                odd++;
+            }
+        }
+        if(odd>even){
+            return even;
+        }
+        else{
+            return odd;
+        }
+        
+    }
+}
