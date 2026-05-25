fafca13fd9da7dea4186b4c2a0e8185b40b9adeb
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-25 15:13:50 +0530

    Time: 2 ms (95.25%) | Memory: 72.2 MB (27.53%) - LeetSync

--- src/arrays/238-product-of-array-except-self/product-of-array-except-self.java
+++ src/arrays/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,21 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
+        // Output array
+        // First it stores exclusive prefix products
+        // Later it gets multiplied with suffix products
         int[] ans = new int[nums.length];
 
+        // For index 0 there are no elements on the left
+        // So exclusive prefix product = 1
         ans[0] = 1;
 
-        for(int i = 1; i<nums.length; i++)
+        // Build exclusive prefix products
+        // ans[i] = product of all elements before i
+        //
+        // Example:
+        // nums = [1,2,3,4]
+        // ans  = [1,1,2,6]
+        for(int i = 1; i < nums.length; i++)
         {
-            ans[i] = ans[i-1] * nums[i-1];
-            
+            // Previous prefix product * previous element
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
 
+        // Stores exclusive suffix product
+        // Initially 1 because last index has no elements on the right
         int right = 1;
-        for(int i = nums.length - 1; i>=0; i--)
+
+        // Traverse from right to left
+        for(int i = nums.length - 1; i >= 0; i--)
         {
+            // Multiply:
+            // exclusive prefix product * exclusive suffix product
             ans[i] *= right;
-            right  *= nums[i]; 
+
+            // Update suffix product for next iteration
+            // moving towards left
+            right *= nums[i];
         }
 
         return ans;
