9cecb6099219498670b011814e59eea35120c57f
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-27 10:14:42 +0530

    Time: 17 ms (15.98%) | Memory: 74.5 MB (21.28%) - LeetSync

---
+++ src/arrays/41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+
+        int[] hash = new int[n+1];
+
+        for(int i = 0; i < n; i++){
+            if(nums[i]>n || nums[i]<=0){
+                nums[i] = n+1;
+            }
+            else{
+                hash[nums[i]] += 1 ;
+            }
+        }
+
+        System.out.println(Arrays.toString(hash));
+
+        for(int i = 1; i<=n; i++){
+            if(hash[i] == 0){
+                return i;
+            }
+        }
+
+        return n+1;
+
+    }
+}
