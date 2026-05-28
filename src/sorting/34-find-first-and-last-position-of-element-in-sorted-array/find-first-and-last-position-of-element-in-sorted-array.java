6c9a4c199bbd2bc630e389f3ac624b1dcb8556a6
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-28 10:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (30.23%) - LeetSync

---
+++ src/sorting/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,65 @@
+class Solution { 
+
+    public int[] searchRange(int[] nums, int target) {
+
+        // Default answer if target is not found
+        int[] ans = {-1, -1};
+
+        // Find first occurrence of target
+        ans[0] = findIndex(nums, target, true);
+
+        // Find last occurrence of target
+        ans[1] = findIndex(nums, target, false);
+
+        return ans;
+    }
+
+    // Binary Search helper function
+    // findStartIndex = true  -> find first occurrence
+    // findStartIndex = false -> find last occurrence
+    public int findIndex(int[] arr, int target, boolean findStartIndex) {
+
+        // Stores the current valid answer
+        int ans = -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+
+            // Prevents integer overflow
+            int mid = start + (end - start) / 2;
+
+            // Target lies on left side
+            if(arr[mid] > target){
+                end = mid - 1;
+            }
+
+            // Target lies on right side
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+
+            // Target found
+            else{
+
+                // Store current index
+                ans = mid;
+
+                // Continue searching on left side
+                // to find first occurrence
+                if(findStartIndex){
+                    end = mid - 1;
+                }
+
+                // Continue searching on right side
+                // to find last occurrence
+                else{
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
