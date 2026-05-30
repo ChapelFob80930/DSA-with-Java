1a191bd5769d1159536e235f8eaafca7822aefce
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-05-30 10:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (72.15%) - LeetSync

--- src/sorting/162-find-peak-element/find-peak-element.java
+++ src/sorting/162-find-peak-element/find-peak-element.java
@@ -4,17 +4,24 @@ class Solution {
         int start = 0;
         int end = arr.length - 1;
 
+        // Binary Search for the peak element
         while (start < end) {
 
             int mid = start + (end - start) / 2;
 
             if (arr[mid] > arr[mid + 1]) {
+                // You are in the descending part of the mountain.
+                // mid can still be the peak, so keep it in the search space.
                 end = mid;
             } else {
+                // You are in the ascending part of the mountain.
+                // Since arr[mid + 1] > arr[mid], the peak must be on the right.
                 start = mid + 1;
             }
         }
 
-        return start;
+        // When the loop ends, start == end.
+        // Both pointers converge to the peak element's index.
+        return start; // or return end;
     }
 }
