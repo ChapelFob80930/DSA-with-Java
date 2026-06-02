25804715358ccf74bd441f004ed7c7b347ec4f64
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-02 22:21:19 +0530

    Time: 2 ms (96.38%) | Memory: 48.6 MB (24.02%) - LeetSync

--- src/sorting/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ src/sorting/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
+
+        // Pointer at the beginning of the sorted array
         int start = 0;
-        int end = numbers.length-1;
 
-        while(start<end){
-            if(numbers[start] + numbers[end] == target){
-                return new int[] {start + 1, end + 1};
+        // Pointer at the end of the sorted array
+        int end = numbers.length - 1;
+
+        // Continue until the two pointers meet
+        while (start < end) {
+
+            // Calculate the sum of the current pair
+            int sum = numbers[start] + numbers[end];
+
+            // If the pair adds up to the target, return 1-based indices
+            if (sum == target) {
+                return new int[] { start + 1, end + 1 };
             }
 
-            else if(numbers[start] + numbers[end] < target){
-                start ++;
+            // If the sum is too small, move start right
+            // to increase the sum
+            else if (sum < target) {
+                start++;
             }
 
-            else if(numbers[start] + numbers[end] > target){
-                end --;
+            // If the sum is too large, move end left
+            // to decrease the sum
+            else {
+                end--;
             }
         }
 
+        // This line will never be reached because
+        // the problem guarantees exactly one solution
         return new int[] {};
-
     }
 }
