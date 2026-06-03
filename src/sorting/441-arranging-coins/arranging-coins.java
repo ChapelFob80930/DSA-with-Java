be21dce0c1a13f88aa5a6dff4d67b5a014e1d8b6
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-04 05:23:02 +0530

    Time: 9 ms (7.47%) | Memory: 42.6 MB (78.26%) - LeetSync

--- src/sorting/441-arranging-coins/arranging-coins.java
+++ src/sorting/441-arranging-coins/arranging-coins.java
@@ -1,13 +1,26 @@
 class Solution {
     public int arrangeCoins(int n) {
+
+        // Counts how many complete rows can be formed
         int cnt = 0;
-        for(int i = 1; i<=n; i++){
-            if((n-i) < 0){
+
+        // Try building rows one by one
+        for (int i = 1; i <= n; i++) {
+
+            // If there are not enough coins to form the current row,
+            // stop the process
+            if ((n - i) < 0) {
                 break;
             }
+
+            // Current row is successfully formed
             cnt++;
+
+            // Remove the coins used in the current row
             n = n - i;
         }
+
+        // Return the number of complete rows formed
         return cnt;
     }
 }
