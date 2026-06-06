876ac7690284d6c38e56cd7117f09255149f9cf7
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-06 07:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (41.43%) - LeetSync

--- src/sorting/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ src/sorting/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,22 +1,46 @@
 class Solution {
     public int countNegatives(int[][] grid) {
+
+        // Start from the top-right corner
         int r = 0;
-        int c = grid[r].length - 1;
+        int c = grid[0].length - 1;
+
         int cnt = 0;
 
-        while(r<grid.length && c >= 0){
-            int ub = grid[r][0];
-            int lb = grid[r][c];
+        // Continue while we're inside the matrix
+        while (r < grid.length && c >= 0) {
 
-            if(lb<0){
-                cnt+=grid.length-r;
+            // If current element is negative
+            if (grid[r][c] < 0) {
+
+                /*
+                 * Since the matrix is sorted column-wise in non-increasing order,
+                 * every element below grid[r][c] in the same column is also negative.
+                 *
+                 * Number of negatives in this column from row r onward:
+                 * grid.length - r
+                 */
+                cnt += grid.length - r;
+
+                // Move left to check the next column
                 c--;
-            }
+            } else {
 
-            else{
+                /*
+                 * Current element is non-negative.
+                 *
+                 * Since the row is sorted in non-increasing order,
+                 * every element to the left is >= current element,
+                 * so they are also non-negative.
+                 *
+                 * Therefore, this row cannot contribute any negatives
+                 * in the current column or to its left.
+                 * Move down to the next row.
+                 */
                 r++;
-            }            
+            }
         }
-        return cnt; 
+
+        return cnt;
     }
 }
