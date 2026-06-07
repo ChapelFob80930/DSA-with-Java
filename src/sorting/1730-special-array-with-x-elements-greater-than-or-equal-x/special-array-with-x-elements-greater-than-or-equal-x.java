d185c9eefea4d92241706c4aad92bdf563a26b0f
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-07 07:00:48 +0530

    Time: 1 ms (83.83%) | Memory: 43 MB (85.49%) - LeetSync

--- src/sorting/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ src/sorting/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,18 +1,26 @@
 class Solution {
     public int specialArray(int[] nums) {
-        int ans = -1;
-        for (int i = 0;i<=nums.length;i++){
-            int cnt = 0;
-            for(int n:nums){
-                if(n>=i){
-                    cnt++;
+
+        // Try every possible value of x from 0 to n
+        for (int x = 0; x <= nums.length; x++) {
+
+            int count = 0;
+
+            // Count how many elements are greater than or equal to x
+            for (int num : nums) {
+                if (num >= x) {
+                    count++;
                 }
             }
-            if(cnt == i && cnt!=0){
-                ans = cnt;
-                return ans;
+
+            // If exactly x elements are >= x,
+            // then x is the special value
+            if (count == x) {
+                return x;
             }
         }
+
+        // No valid x found
         return -1;
     }
 }
