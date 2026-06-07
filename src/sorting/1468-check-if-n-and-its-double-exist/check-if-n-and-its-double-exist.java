d71b2671e74ed358b6f00045b518261e6bae3993
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-07 06:44:06 +0530

    Time: 2 ms (97.72%) | Memory: 45.1 MB (14.11%) - LeetSync

--- src/sorting/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ src/sorting/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashMap<Integer, Integer> doubles = new HashMap<>();
-
-        for(int i = 0; i<arr.length; i++){
-            doubles.put(2*arr[i],i);
-        }
-
-        for(int i = 0; i<arr.length; i++){
-
-            var j = doubles.get(arr[i]);
-
-            if(j!=null && j!=i)
-            {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n:arr){
+            if(set.contains(2*n) || (n%2==0 && set.contains(n/2))){
                 return true;
             }
+            set.add(n);
         }
 
         return false;
