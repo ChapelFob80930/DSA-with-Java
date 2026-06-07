cd39b86a8a43f5b7e5a67455b915f56917e21786
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-07 06:13:45 +0530

    Time: 9 ms (52.83%) | Memory: 48.6 MB (32.94%) - LeetSync

--- src/sorting/924-fair-candy-swap/fair-candy-swap.java
+++ src/sorting/924-fair-candy-swap/fair-candy-swap.java
@@ -1,29 +1,51 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+
+        // Store Bob's candy sizes for O(1) lookups
         HashSet<Integer> newBobSizes = new HashSet<>();
 
-        for(int i:bobSizes){
+        for (int i : bobSizes) {
             newBobSizes.add(i);
         }
 
+        /*
+         * Let Alice give x and receive y.
+         *
+         * After swap:
+         * sumA - x + y = sumB - y + x
+         *
+         * Rearranging:
+         * y = x + (sumB - sumA) / 2
+         *
+         * So for every candy x that Alice can give,
+         * we only need to check whether the required y
+         * exists in Bob's collection.
+         */
         int diff = (sum(bobSizes) - sum(aliceSizes)) / 2;
 
-        for(int x: aliceSizes){
-            int y = x+diff;
-            if(newBobSizes.contains(y))
-            {
-                return new int[] {x,y};
+        for (int x : aliceSizes) {
+
+            // Required candy size from Bob to make totals equal
+            int y = x + diff;
+
+            // If Bob has the required candy, return the swap pair
+            if (newBobSizes.contains(y)) {
+                return new int[] { x, y };
             }
         }
 
-        return new int[] {-1, -1};
+        // Problem guarantees an answer, but added as a safeguard
+        return new int[] { -1, -1 };
     }
 
-    public int sum(int arr[]){ 
-        int sum = 0; 
-        for(int i:arr){
-            sum+=i; 
-        } 
-            return sum; 
-    }    
+    // Returns the total number of candies in an array
+    public int sum(int arr[]) {
+        int sum = 0;
+
+        for (int i : arr) {
+            sum += i;
+        }
+
+        return sum;
+    }
 }
