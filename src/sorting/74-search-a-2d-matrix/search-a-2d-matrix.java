72296eee54d0bb54343dcd16fe49519be4ce0be8
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-12 10:07:14 +0530

    Time: 2 ms (1.96%) | Memory: 44.1 MB (23.72%) - LeetSync

--- src/sorting/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ src/sorting/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,24 +1,56 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int r = 0;
-        int c = matrix[0].length-1;
-        
-        while(r<matrix.length && c>=0){
+        int solRow = findRow(matrix, target);
+        System.out.println(solRow);
+        if(solRow == -1){
+            return false;
+        }
+        return binarySearch(matrix[solRow], target);
+    }
 
-            if(matrix[r][c] == target){
+    public int findRow(int[][] matrix, int target){
+        int top = 0;
+        int bottom = matrix.length - 1;
+
+        while(top<=bottom){
+            int row = top + (bottom - top) / 2;            
+            
+            if(matrix[row][0] > target){
+                bottom = row - 1;
+            }
+
+            else if(matrix[row][matrix[row].length - 1] < target){
+                top = row + 1;
+            }
+
+            else{
+                return row;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
                 return true;
             }
 
-            else if(matrix[r][c] > target){
-                c--;
+            else if(arr[mid] > target){
+                end = mid - 1;
             }
 
-            else if(matrix[r][c] < target){
-                r++;
+            else{
+                start = mid + 1;
             }
         }
 
         return false;
-        
     }
 }
