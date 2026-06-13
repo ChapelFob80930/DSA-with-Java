410fb33219afe8989747ac6956cebb04edbca4d9
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-13 10:11:34 +0530

    Time: 10 ms (85.63%) | Memory: 49.8 MB (92.14%) - LeetSync

--- src/sorting/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ src/sorting/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,70 +1,99 @@
 class Solution {
+
     public int shipWithinDays(int[] weights, int days) {
+
+        // Total weight of all packages.
+        // This represents the maximum possible ship capacity
+        // (ship everything in one day).
         int totalWeight = sum(weights);
-        
+
+        // Minimum possible capacity must be at least the
+        // heaviest package, otherwise that package can never be shipped.
         int start = max(weights);
+
+        // Maximum possible capacity is the sum of all packages.
         int end = totalWeight;
 
-        while(start<end){
-            int mid = start + (end - start)/2;
+        // Binary search for the smallest valid ship capacity.
+        while (start < end) {
 
-            System.out.println("Start: "+start);
-            System.out.println("mid: "+mid);
-            System.out.println("end: "+end);
+            int mid = start + (end - start) / 2;
 
-
-            if(checkIfValidWeightOfShip(weights, mid, totalWeight, days)){
+            // If this capacity can ship all packages within
+            // the given number of days, try a smaller capacity.
+            if (checkIfValidWeightOfShip(weights, mid, days)) {
                 end = mid;
             }
 
-            else{
+            // Otherwise capacity is too small,
+            // search in the larger half.
+            else {
                 start = mid + 1;
             }
         }
 
+        // start == end => smallest valid capacity.
         return start;
     }
 
-    public int sum(int[] weights){
+    // Returns the sum of all package weights.
+    public int sum(int[] weights) {
         int sum = 0;
-        for(int w : weights){
+
+        for (int w : weights) {
             sum += w;
         }
+
         return sum;
     }
 
-    public int max(int[] weights){
+    // Returns the weight of the heaviest package.
+    public int max(int[] weights) {
         int max = Integer.MIN_VALUE;
-        for(int w : weights){
-            if(w>max){
+
+        for (int w : weights) {
+            if (w > max) {
                 max = w;
             }
         }
+
         return max;
     }
 
-    public boolean checkIfValidWeightOfShip(int[] weights, int shipCapacity, int totalWeight, int days){
+    /*
+     * Simulates shipping packages using a ship of capacity shipCapacity.
+     *
+     * Greedy approach:
+     * - Keep loading packages into the current day while they fit.
+     * - As soon as the next package doesn't fit, start a new day.
+     *
+     * If the total days required <= allowed days,
+     * then this ship capacity is valid.
+     */
+    public boolean checkIfValidWeightOfShip(int[] weights,
+                                            int shipCapacity,
+                                            int days) {
 
         int currentLoad = 0;
+
+        // We always start with Day 1.
         int daysUsed = 1;
-        int loaded = 0;
-        
-        for(int w: weights){
-            if(currentLoad + w <= shipCapacity){
-                currentLoad+=w;
+
+        for (int w : weights) {
+
+            // Package fits in the current day's shipment.
+            if (currentLoad + w <= shipCapacity) {
+                currentLoad += w;
             }
-            else{
-                loaded += currentLoad; 
-                daysUsed ++;
+
+            // Package doesn't fit.
+            // Start a new day and place this package there.
+            else {
+                daysUsed++;
                 currentLoad = w;
             }
-
         }
 
-        if(daysUsed <= days){
-            return true;
-        }
-
-        return false;
+        return daysUsed <= days;
     }
 }
