12b638b2eba66aeffca88510dfda6de2bdb6b2e7
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-15 22:24:00 +0530

    Time: 22 ms (27.88%) | Memory: 80.4 MB (80.54%) - LeetSync

---
+++ src/sorting/217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n:nums){
+            set.add(n);
+        }
+
+        if(set.size()<nums.length){
+            return true;
+        }
+
+        return false;
+    }
+}
