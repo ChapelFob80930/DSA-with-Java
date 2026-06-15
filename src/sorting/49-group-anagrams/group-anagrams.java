18ddc29b67cf37c8f14bacf58b9747eeb3d7e450
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-15 23:08:22 +0530

    Time: 6 ms (99.42%) | Memory: 49.4 MB (91.45%) - LeetSync

--- src/sorting/49-group-anagrams/group-anagrams.java
+++ src/sorting/49-group-anagrams/group-anagrams.java
@@ -1,43 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
 
-        for(int i = 0; i<strs.length; i++){
-            List<String> temp = new ArrayList<>();
-            if("-1".equals(strs[i])){
-                continue;
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            char[] arr = s.toCharArray();
+
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
-            temp.add(strs[i]);
-            for(int j = i+1; j<strs.length; j++){
-                if(!"-1".equals(strs[j]) && checkIfAnagram(strs[i],strs[j])){
-                    temp.add(strs[j]);
-                    strs[j] = "-1";
-                }
-            }
-            ans.add(temp);
+
+            map.get(key).add(s);
         }
 
-        return ans;
-    }
-
-    public boolean checkIfAnagram(String s, String t){
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int cnt : count) {
-            if (cnt != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return new ArrayList<>(map.values());
     }
 }
