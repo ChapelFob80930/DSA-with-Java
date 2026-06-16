9720a29ed980b38e46808584ce8a9e1ac9da89f4
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-16 07:06:05 +0530

    Time: 15 ms (55.32%) | Memory: 47.3 MB (94.98%) - LeetSync

--- src/sorting/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ src/sorting/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
+        // Store frequency of each number
         Map<Integer, Integer> count = new HashMap<>();
 
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
+        // Store [frequency, number] pairs
         ArrayList<int[]> arr = new ArrayList<>();
 
-        for(Map.Entry<Integer, Integer> entry : count.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
             arr.add(new int[] {entry.getValue(), entry.getKey()});
         }
 
-        arr.sort((a,b)->b[0]-a[0]);
+        // Sort in descending order of frequency
+        // Example: [5,7] comes before [3,2]
+        arr.sort((a, b) -> b[0] - a[0]);
 
         int[] ans = new int[k];
 
+        // Take the first k numbers (most frequent)
         for (int i = 0; i < k; i++) {
             ans[i] = arr.get(i)[1];
         }
-        
+
         return ans;
     }
 }
