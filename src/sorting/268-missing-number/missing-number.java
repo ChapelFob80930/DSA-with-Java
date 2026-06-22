7a938bea1d12d3fb309ffc2c534cc45c04b7c2e6
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-22 08:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (95.23%) - LeetSync

--- src/sorting/268-missing-number/missing-number.java
+++ src/sorting/268-missing-number/missing-number.java
@@ -1,64 +1,13 @@
+//amazon
 class Solution {
-
-    public int missingNumber(int[] arr) {
-
-        cycle_sort(arr);
-        
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] != i){
-                return i;
-            }
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = (n + 1) * n / 2;
+        int actualSum = 0;
+        for(int num: nums){
+            actualSum += num;
         }
 
-        return arr.length;
-    }
-
-
-    public void cycle_sort(int[] arr){
-
-        int i = 0;
-
-        while(i < arr.length){
-
-            if(arr[i]<arr.length)
-            {
-                int correct = arr[i];
-
-                if(arr[i] != arr[correct]){
-
-                    swap(arr, i, correct);
-                }
-
-                else{
-
-                    i++;
-                }
-            }
-
-            else{
-                i++;
-            }
-        }
-    }
-
-    public static void swap(int[] arr, int index1, int index2){
-
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+        return expectedSum - actualSum;
     }
 }
-
-//alternate solution
-//class Solution {
-//    public int missingNumber(int[] nums) {
-//        int n = nums.length;
-//        int expectedSum = (n + 1) * n / 2;
-//        int actualSum = 0;
-//        for(int num: nums){
-//            actualSum += num;
-//        }
-//
-//        return expectedSum - actualSum;
-//    }
-//}
