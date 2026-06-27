83a58fa987690da8b519ff4c66e714b6e58b9ad4
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-27 10:52:33 +0530

    Time: 77 ms (9.31%) | Memory: 46.6 MB (20.44%) - LeetSync

---
+++ src/arrays_leetcode_questions/125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+        while(l<r){
+            System.out.println("Start of loop");
+            while(l<r && !isAlphaNum(s.charAt(l))){
+                l++;
+            }
+
+            while(r>l && !isAlphaNum(s.charAt(r))){
+                r--;
+            }
+
+            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                return false;
+            }            
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    public boolean isAlphaNum(char c){
+        if(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || '0' <= c && c <= '9'){
+            return true;
+        } 
+
+        return false;
+    }
+}
