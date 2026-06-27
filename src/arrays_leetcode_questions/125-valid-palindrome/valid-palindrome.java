0d14c530a282e3a837dc970b8dcae016439f6df6
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-06-27 10:53:15 +0530

    Time: 2 ms (99.23%) | Memory: 44.6 MB (51.79%) - LeetSync

--- src/arrays_leetcode_questions/125-valid-palindrome/valid-palindrome.java
+++ src/arrays_leetcode_questions/125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,38 @@
+/*
+--------------------------------------------------
+Two Pointers + Character Validation Problem
+--------------------------------------------------
+Idea:
+- Use two pointers from both ends.
+- Skip all non-alphanumeric characters.
+- Compare characters case-insensitively.
+- If any mismatch occurs, it is not a palindrome.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length()-1;
-        while(l<r){
-            while(l<r && !isAlphaNum(s.charAt(l))){
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+
+            // Skip non-alphanumeric characters from the left
+            while (l < r && !isAlphaNum(s.charAt(l))) {
                 l++;
             }
 
-            while(r>l && !isAlphaNum(s.charAt(r))){
+            // Skip non-alphanumeric characters from the right
+            while (r > l && !isAlphaNum(s.charAt(r))) {
                 r--;
             }
 
-            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+            // Compare characters ignoring case
+            if (Character.toLowerCase(s.charAt(l)) !=
+                Character.toLowerCase(s.charAt(r))) {
                 return false;
-            }            
+            }
 
             l++;
             r--;
@@ -21,10 +41,13 @@ class Solution {
         return true;
     }
 
-    public boolean isAlphaNum(char c){
-        if(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || '0' <= c && c <= '9'){
+    // Checks whether a character is a letter or digit
+    public boolean isAlphaNum(char c) {
+        if (('A' <= c && c <= 'Z') ||
+            ('a' <= c && c <= 'z') ||
+            ('0' <= c && c <= '9')) {
             return true;
-        } 
+        }
 
         return false;
     }
