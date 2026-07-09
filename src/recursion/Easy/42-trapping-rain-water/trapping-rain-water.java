c1a68ea8635bb74199178248d5279e9be0d04ec9
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-07-09 12:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (56.87%) - LeetSync

--- src/recursion/Easy/42-trapping-rain-water/trapping-rain-water.java
+++ src/recursion/Easy/42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,170 @@
 class Solution {
+
+    /*
+     * TRAPPING RAIN WATER - TWO POINTERS
+     *
+     * Key Formula:
+     *
+     * waterAtIndex =
+     * min(max wall on left, max wall on right)
+     * - currentHeight
+     *
+     * -------------------------------------------------
+     * VISUALIZATION
+     * -------------------------------------------------
+     *
+     * Example:
+     *
+     * height = [4,2,0,3,2,5]
+     *
+     *         █
+     *         █
+     * █       █
+     * █ ~ ~ █ █
+     * █ ~ ~ █ █
+     * █ █ █ █ █
+     * ----------
+     *
+     * '~' = trapped water
+     *
+     * Water at each index:
+     *
+     * index 1:
+     * 4 - 2 = 2
+     *
+     * index 2:
+     * 4 - 0 = 4
+     *
+     * index 3:
+     * 4 - 3 = 1
+     *
+     * index 4:
+     * 4 - 2 = 2
+     *
+     * Total = 9
+     *
+     * -------------------------------------------------
+     * CORE OBSERVATION
+     * -------------------------------------------------
+     *
+     * Water level is determined by the SMALLER boundary.
+     *
+     * Example:
+     *
+     * leftMax = 4
+     * rightMax = 7
+     *
+     * 4            7
+     * █            █
+     * █            █
+     * █    ?       █
+     *
+     * Water can rise only to height 4.
+     *
+     * Therefore:
+     *
+     * min(4,7) = 4
+     *
+     * The taller wall does not matter.
+     *
+     * -------------------------------------------------
+     * WHY TWO POINTERS WORK
+     * -------------------------------------------------
+     *
+     * If:
+     *
+     * leftMax <= rightMax
+     *
+     * then:
+     *
+     * min(leftMax,rightMax)
+     * =
+     * leftMax
+     *
+     * So the water on the left side is already known.
+     *
+     * We do NOT need more information from the right.
+     *
+     * Therefore process left side.
+     *
+     * Similarly:
+     *
+     * rightMax < leftMax
+     *
+     * means the right side is already determined.
+     *
+     * Therefore process right side.
+     *
+     * -------------------------------------------------
+     * DRY RUN
+     * -------------------------------------------------
+     *
+     * height = [4,2,0,3,2,5]
+     *
+     * l=0 r=5
+     * leftMax=4 rightMax=5
+     *
+     * leftMax <= rightMax
+     *
+     * Move l -> 1
+     * Water = 4 - 2 = 2
+     *
+     * Move l -> 2
+     * Water = 4 - 0 = 4
+     *
+     * Move l -> 3
+     * Water = 4 - 3 = 1
+     *
+     * Move l -> 4
+     * Water = 4 - 2 = 2
+     *
+     * Total = 9
+     *
+     * -------------------------------------------------
+     * REVISION LINE
+     * -------------------------------------------------
+     *
+     * Smaller boundary determines water level.
+     * Process the side whose maximum wall is smaller.
+     *
+     * Time  : O(n)
+     * Space : O(1)
+     */
+
     public int trap(int[] height) {
-        int leftMax = height[0];
-        int rightMax = height[height.length-1];
+
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
         int l = 0;
-        int r = height.length-1;
+        int r = height.length - 1;
+
+        int leftMax = height[l];
+        int rightMax = height[r];
+
         int res = 0;
 
-        while(l<r){
-            if(leftMax <= rightMax){
-                l++;
-                leftMax = Math.max(leftMax, height[l]);
-                int waterAtIndex = leftMax - height[l];
-                res += waterAtIndex;
-            }
+        while (l < r) {
 
-            else{
+            if (leftMax <= rightMax) {
+
+                l++;
+
+                leftMax = Math.max(leftMax, height[l]);
+
+                res += leftMax - height[l];
+
+            } else {
+
                 r--;
+
                 rightMax = Math.max(rightMax, height[r]);
-                int waterAtIndex = rightMax - height[r];
-                res += waterAtIndex;
+
+                res += rightMax - height[r];
             }
         }
 
         return res;
-        
     }
 }
