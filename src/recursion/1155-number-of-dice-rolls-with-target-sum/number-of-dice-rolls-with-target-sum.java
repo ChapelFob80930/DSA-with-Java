cdc86f2e3f92e3c0fecf3ecf9d77b172d78a043e
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-07-28 10:02:05 +0530

    Time: 38 ms (5.10%) | Memory: 44 MB (25.50%) - LeetSync

--- src/recursion/1155-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ src/recursion/1155-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,34 +1,31 @@
 class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         Integer[][] dp = new Integer[n+1][target+1];
-        return countNumRolls(n, k, target, 0, dp);
+        return countNumRolls(n, k, target, dp);
     }
 
-    public int countNumRolls(int n, int k, int target, int num, Integer[][] dp){
+    public int countNumRolls(int n, int k, int target, Integer[][] dp){
         
-        if(num > target){
-            return 0;
-        }
-        
-        if(n == 0){
-            if(num == target){
-                return 1;
-            }
+        if(target<0){
             return 0;
         }
 
-        if(dp[n][num] != null){
-            return dp[n][num];
+        if(n == 0){
+            return target == 0 ? 1 : 0;
+        }
+
+        if(dp[n][target] != null){
+            return dp[n][target];
         }
 
         long cnt = 0;
 
         for(int i = 1; i<=k; i++){
-            cnt = (cnt + countNumRolls(n-1, k, target, num + i, dp)) % (long)(Math.pow(10, 9) + 7);
+            cnt = (cnt + countNumRolls(n-1, k, target - i, dp)) % (long)(Math.pow(10, 9) + 7);
         }
 
-        dp[n][num] = (int)(cnt);
+        dp[n][target] = (int)(cnt);
 
-        return dp[n][num];
+        return dp[n][target];
     }
 }
